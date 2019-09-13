389e997c465e4e1c380e753e9f4ab5ae9f441c43
awais000 <awaismanzoor00@gmail.com>
2019-09-13 13:04:11 +0500

    Set up and structure project

---
+++ src/pages/dashboard/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card, Typography, Alert } from 'antd';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+
+
+export default () => (<div></div>
+  // <PageHeaderWrapper>
+   
+  // </PageHeaderWrapper>
+);
